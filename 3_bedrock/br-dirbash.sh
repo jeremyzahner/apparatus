27a44b269beb6523cd706cddbd37a96a88e30b83
Jeremy Zahner <me@jeremyzahner.com>
2014-10-23 16:17:48 +0200

    Revert de3fe90..a3eb0d3
    This rolls back to commit de3fe90b956ecf54e1f54fbf04fada71926ba13d.

---
+++ 3_bedrock/br-dirbash.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+############################################################################
+#	Created by Jeremy "Jay" Zahner (@jeremyzahner)
+#  
+#	Alpha Release
+#
+#	Version: 0.3.0 - Relying on .env files now for variables.
+############################################################################
+
+## CONFIGURATION
+source .env
+#
+if [ ! -f .env]
+then
+	echo $'\n''Cant find your current projects .env file. Check wether you are in your projects root and the .env file exists.'
+	#
+	exit
+fi
+#
+echo $'\n''Script starting...'
+###########
+echo $'\n''The Wordpress directory is '$DEVELOPMENT_WP_PATH
+echo $'\n''Your Uploads/Assets are stored in '$DEVELOPMENT_UPLOADS_PATH'.'$'\n'
+###########
+if [ ! -d $DEVELOPMENT_UPLOADS_PATH ]
+then
+	sudo mkdir $DEVELOPMENT_UPLOADS_PATH
+fi
+###########
+sudo chown -R $DEVELOPMENT_USER:$DEVELOPMENT_GROUP ./*
+sudo find $DEVELOPMENT_PATH/* -type f -exec chmod 664 {} \; 
+sudo find $DEVELOPMENT_PATH/* -type d -exec chmod 775 {} \;
+sudo find $DEVELOPMENT_PATH/* -type d -exec chmod g+s {} \;
+#
+echo $'\n''All went well. Thanks for using!'$'\n'
+#
+exit
